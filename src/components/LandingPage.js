60673efebf94793fed90b02d26639e47135cd334
Roberto Santos <robot-o@hotmail.com>
2019-01-17 11:58:56 -0800

    add Header and LandingPage components

---
+++ src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => (
+    <div className="jumbotron">
+        <h1 className="display-3">Welcome to AccessCO</h1>
+        <p className="lead">This app will allow you to check on accessibility at establishments, facilities and natural features in Colorado.</p>
+        <hr className="my-4"/>
+        <p>Some of the facilities listed are built-up and maintained, some are naturally-occurring and unmaintained.</p>
+        <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+);
+
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootswatch/dist/lux/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
