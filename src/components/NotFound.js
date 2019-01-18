348ee6d27cd43ed4eb772b9ff736f0058a6f8e65
Roberto Santos <robot-o@hotmail.com>
2019-01-18 12:22:45 -0800

    add Switch and NotFound component

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+    <div class="alert alert-warning">
+  <h4 class="alert-heading">Not found</h4>
+  <p class="mb-0">We couldn't find what you were looking for.</p>
+</div>
+);
+
+export default NotFound;
