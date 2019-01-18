139eb3d28e79058f35b5f70429d80685e68a1767
Roberto Santos <robot-o@hotmail.com>
2019-01-18 12:12:43 -0800

    add React Router

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,9 +5,9 @@ const LandingPage = () => (
         <h1 className="display-3">Welcome to AccessCO</h1>
         <p className="lead">This app will allow you to check on accessibility at establishments, facilities and natural features in Colorado.</p>
         <hr className="my-4"/>
-        <p>Some of the facilities listed are built-up and maintained, some are naturally-occurring and unmaintained.</p>
+        <p>Some of the facilities listed are built-up and maintained, some are naturally-occurring and unmaintained. This app also allows you to update information about existing records, as well as add information about establishments not listed.</p>
         <p className="lead">
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Search!</a>
         </p>
     </div>
 );
